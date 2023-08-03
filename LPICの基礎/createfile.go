bd145a87e4a68a9e1b3883f294347f95e34b4f68
General0917 <ht13a037@oecu.jp>
2023-08-04 05:29:02 +0900

    ァイル作成ースコードを修正

--- LPICの基礎/createfile.go
+++ LPICの基礎/createfile.go
@@ -10,11 +10,17 @@ func main() {
 		fileName := fmt.Sprintf("%d日目.md", i)
 		fileContent := fmt.Sprintf("# Day %d\n\nWrite your content here for day %d.", i, i)
 
-		err := os.WriteFile(fileName, []byte(fileContent), 0644)
-		if err != nil {
-			fmt.Println("Error creating file:", err)
+		// ファイルが既に存在するか確認
+		if _, err := os.Stat(fileName); os.IsNotExist(err) {
+			// ファイルが存在しない場合のみ作成
+			err := os.WriteFile(fileName, []byte(fileContent), 0644)
+			if err != nil {
+				fmt.Println("Error creating file:", err)
+			} else {
+				fmt.Println("File created:", fileName)
+			}
 		} else {
-			fmt.Println("File created:", fileName)
+			fmt.Println("File already exists, skipping:", fileName)
 		}
 	}
 }
